0e5e6292eb8496123a2e14340de31882632b0e4d
RithickDharmaRaj <rithick.darkcoder@gmail.com>
2021-05-19 04:57:25 +0530

    Made as calling from one file to another.

--- practice.py
+++ practice.py
@@ -1,6 +1,7 @@
+# Printing Prime Numbers...
 start,end = input("Enter the starting and ending numbers : ").split()
-div_num =  int(input("Enter the number which divides : "))
-print(f"The numbers divisible by {div_num} from '{start}' to '{end}' are :")
+print(f"The Prime Numbers from {start} to {end} are : ")
 for numbers in range(int(start),int(end)):
     if numbers % numbers == 0 and numbers % 2 != 0 and numbers % 3 != 0 and numbers % 5 != 0 or numbers == 2:
         print(numbers)
+print("Thank You!","\n","          -darkCoder")

1d90c162870863a756528e25eacc0800739205c2
RithickDharmaRaj <rithick.darkcoder@gmail.com>
2021-05-20 02:35:14 +0530

    Changes in range values...

--- practice.py
+++ practice.py
@@ -1,7 +1,7 @@
 # Printing Prime Numbers...
 start,end = input("Enter the starting and ending numbers : ").split()
 print(f"The Prime Numbers from {start} to {end} are : ")
-for numbers in range(int(start),int(end)):
+for numbers in range(int(start),int(end) + 1):
     if numbers % numbers == 0 and numbers % 2 != 0 and numbers % 3 != 0 and numbers % 5 != 0 or numbers == 2 or numbers == 3 or numbers == 5:
         if numbers == 1:
             continue

dce139b8f35587c28213b798df0953f93f44c889
RithickDharmaRaj <rithick.darkcoder@gmail.com>
2021-05-19 14:40:50 +0530

    prime local code

--- practice.py
+++ practice.py
@@ -2,7 +2,9 @@
 start,end = input("Enter the starting and ending numbers : ").split()
 print(f"The Prime Numbers from {start} to {end} are : ")
 for numbers in range(int(start),int(end)):
-    if numbers % numbers == 0 and numbers % 2 != 0 and numbers % 3 != 0 and numbers % 5 != 0 or numbers == 2:
+    if numbers % numbers == 0 and numbers % 2 != 0 and numbers % 3 != 0 and numbers % 5 != 0 or numbers == 2 or numbers == 3 or numbers == 5:
+        if numbers == 1:
+            continue
         print(numbers)
 
 #Personal Greetings...

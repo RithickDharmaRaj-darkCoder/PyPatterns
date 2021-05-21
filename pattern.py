d83cc91727f88f950e125f7b9ef7a36ed1a58818
RithickDharmaRaj <rithick.darkcoder@gmail.com>
2021-05-21 23:37:41 +0530

    pattern 2 added...

--- pattern.py
+++ pattern.py
@@ -7,5 +7,11 @@ if number == 0:
     pattern0()
 elif number == 1:
     pattern1()
+elif number == 2:
+    pattern2()
 else:
     print("No pattern found!")
+
+
+#Personal Greetings...
+print("\nThank You!\n          -darkCoder \U0001F43E")
--- ptrn_lst.py
+++ ptrn_lst.py
@@ -1,21 +1,29 @@
 # pattern files...
 
-def pattern0():
+def pattern0(): #square...
     sq = input("Enter one element : ").upper()
    # r = int(input("Enter no.of rows : "))
    # c = int(input("Enter no.of columns : "))
     for row in range(5):
+        print("     ", end=" ")
         for column in range(5):
             print(sq,end=" ")
         print()
 
-def pattern1():
+def pattern1(): #left L triangle...
     sq = input("Enter one element : ").upper()
     # r = int(input("Enter no.of rows : "))
     for row in range(5):
+        print("     ", end=" ")
         for column in range(row+1):
             print(sq,end=" ")
         print()
 
-def pattern2():
-    print()
+def pattern2(): #invert left L triangle...
+    sq = input("Enter one element : ").upper()
+    # r = int(input("Enter no.of rows : "))
+    for row in range(5):
+        print("     ",end=" ")
+        for column in range(5-row):
+            print(sq, end=" ")
+        print()

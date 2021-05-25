164093c05d44901e132c50121bcafa3d73ea61ef
RithickDharmaRaj <rithick.darkcoder@gmail.com>
2021-05-26 00:58:21 +0530

    Pattern 10,11 added...

--- R_tri_ptrn.py
+++ R_tri_ptrn.py
@@ -3,8 +3,27 @@
 def pattern9():
     # sq = int(input("How many rows needed? : "))
     for row in range(5):
+        print("     ", end=" ")
         for space in range(5 - row - 1):
             print(" ", end=" ")
         for column in range(row + 1):
             print(column + 1, end=" ")
+        print()
+
+def pattern10():
+    for row in range(5):
+        print("     ", end=" ")
+        for space in range(5 - row - 1):
+            print(" ", end=" ")
+        for column in range(row + 1):
+            print(row + 1 - column, end=" ")
+        print()
+
+def pattern11():
+    for row in range(5):
+        print("     ", end=" ")
+        for space in range(5 - row - 1):
+            print(" ", end=" ")
+        for column in range(row + 1):
+            print(row + 1, end=" ")
         print()

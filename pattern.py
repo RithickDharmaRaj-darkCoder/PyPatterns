e743cf9959866c0b5f997015520b22ec7a363885
RithickDharmaRaj <rithick.darkcoder@gmail.com>
2021-06-02 01:51:20 +0530

    Pattern 25 is added...

--- pattern.py
+++ pattern.py
@@ -6,7 +6,7 @@ from alpha_ptrn import  *
 
 def patterns():
     try:
-        number = int(input("Enter one number [0-24] : "))
+        number = int(input("Enter one number [0-25] : "))
         if number == 0:
             pattern0()
         elif number == 1:
@@ -57,6 +57,8 @@ def patterns():
             pattern23()
         elif number == 24:
             pattern24()
+        elif number == 25:
+            pattern25()
         elif number == 100:
             a()
         elif number == 101:
--- shapes_ptrn.py
+++ shapes_ptrn.py
@@ -166,3 +166,22 @@ def pattern24():  # Even Triangle ...
             print()
     else:
         print("Warning! Enter only '1' Character.")
+
+def pattern25():  # Inverted odd Triangle...
+    sq = input("Enter one element : ").upper()
+    if len(sq) == 1:
+        print()
+        # r = int(input("Enter no.of rows : "))
+        c = 0
+        s = 9
+        for row in range(s):
+            print("    ", end="")
+            for space in range(9 - s):
+                print(end="  ")
+            for column in range(9 - c):
+                print(sq + " ", end="  ")
+            c += 2
+            s -= 2
+            print()
+    else:
+        print("Warning! Enter only '1' Character.")

4e47b746c23f544320602f06ad43f0a3f98e7ce8
RithickDharmaRaj <rithick.darkcoder@gmail.com>
2021-06-10 23:16:47 +0530

    Pattern 32 is added...

--- pattern.py
+++ pattern.py
@@ -6,7 +6,7 @@ from alpha_ptrn import  *
 
 def patterns():
     try:
-        number = int(input("Enter one number [0-31] : "))
+        number = int(input("Enter one number [0-32] : "))
         if number == 0:
             pattern0()
         elif number == 1:
@@ -71,6 +71,8 @@ def patterns():
             pattern30()
         elif number == 31:
             pattern31()
+        elif number == 32:
+            pattern32()
         elif number == 100:
             a()
         elif number == 101:
